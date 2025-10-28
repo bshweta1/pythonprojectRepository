f60855254610afd11db27e23691d0c9a818daac5
bshweta1 <bshweta3110@gmail.com>
2025-10-28 12:02:39 +0530

    Add files via upload
    random heads or tails generate after roll outing dice

---
+++ HeadsOrTails.py
@@ -0,0 +1,13 @@
+import random
+
+# random_integer=random.randint(1,10)
+# print(random_integer)           #to generate random integer
+#
+# random_float=random.uniform(1,10)
+# print(random_float)   # to generate random
+
+random_heads_or_tails=random.randint(0,1)
+if(random_heads_or_tails==0):
+    print("Heads")
+else:
+    print("Tails")
